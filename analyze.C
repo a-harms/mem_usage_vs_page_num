2547df8819c645d501e424877123d7c1a975d3c4
Anna Harms <anna.helena.harms@cern.ch>
2026-07-23 22:30:49 +0200

    Update analyze.C macro to parse run group record csv file and load into a map of structs.

--- analyze.C
+++ analyze.C
@@ -18,32 +18,70 @@
 #include <sstream>
 #include <utility>
 
+#include <map>
 
-
+struct runInfo {
+   int numFields;
+   int numEntries;
+};
 
 void AnalyzeResults(std::string csvRunGroupRecordName) {
-   // Create a dataframe from the RunGroupRecord record csv file
-   auto df = ROOT::RDF::FromCSV(csvRunGroupRecordName);
+   // Creating empty map for accessing run information using run number
+   map<int, runInfo> runNumToRunInfo;
 
-   //std::list<int> entryNums;
+   // Strings and characters for parsing csv file
+   std::string line;
+   std::string cell;
+   char delimiter = ',';
 
+   // variables for temporarily holding csv values
+   int runNum;
+   int numFields;
+   int numEntries;
 
-   // Get the numEvents column values as an RVec collection
-   auto entryNumColumnVec = df.Take<int>("numEvents");
+   // Initialize input file stream for run group record csv file
+   std::ifstream csvRunGroupRecord(csvRunGroupRecordName);
 
-   // Get a set of the numEvents column entries
-   auto entryNumSet = std::set(entryNumColumnVec.begin(), entryNumColumnVec.end());
+   // Remove header row from group record csv file
+   std::getline(csvRunGroupRecord, line);
 
-   // Iterate over the different numEvents entries
-   for (auto entryNum : entryNumSet) {
-      std::cout << entryNum << std::endl;
-      
+   // Read in and parse the lines of the run group record csv and populate the runNumToRunInfo map
+   while (csvRunGroupRecord.good()) {
+      if ((csvRunGroupRecord.is_open())) {
+         // Create a struct to be save run information for the current run
+         runInfo tempInfoLoader;
+
+         // Get the next line from the run group record csv file and use it to make a string stream
+         while (std::getline(csvRunGroupRecord, line)) {
+            // Initialize string stream from line in order to extract cell values
+            std::stringstream lineStream(line);
+
+            // Extract the csv cell values from the run group record file and save to the appropriate variables for later use
+            std::getline(lineStream, cell, delimiter);
+            runNum = std::stoi(cell);
+
+            std::getline(lineStream, cell, delimiter);
+            numFields = std::stoi(cell);
+
+            std::getline(lineStream, cell, delimiter);
+            numEntries = std::stoi(cell);
+
+            // Load the extracted values into the temporary struct
+            tempInfoLoader.numFields = numFields;
+            tempInfoLoader.numEntries = numEntries;
+
+            // Add new element with run information to runNumToRunInfo map with runNum as key
+            runNumToRunInfo[runNum] = tempInfoLoader;
+         }
+
+      } else {
+         std::cout << "Run Group Record CSV file not open." << std::endl;
+     }
    }
-
 }
 
 
 
 void analyze(std::string csvRunGroupRecordName, std::string rw) {
-   AnalyzeResults()
+   AnalyzeResults(csvRunGroupRecordName);
 }
