d4ac15355f027eab3d4dcf2598f47030c5c301d5
Anna Harms <anna.helena.harms@cern.ch>
2026-08-03 11:06:56 +0200

    Update analyze.C to center plot text

--- analyze.C
+++ analyze.C
@@ -197,6 +197,8 @@ void GenerateGroupPlots(int groupNum, map<int, runInfo> runNumToRunInfo) {
    gr1->SetTitle("Average Maximum RSS Values");
    gr1->GetXaxis()->SetTitle("Number of pages per file");
    gr1->GetYaxis()->SetTitle("Average maximum RSS for parameter set (MiB)");
+   gr1->GetXaxis()->CenterTitle(true);
+   gr1->GetYaxis()->CenterTitle(true);
    gr1->Draw("ALP");
 
    // Plot the average maximum vss versus the number of pages per file
@@ -205,6 +207,8 @@ void GenerateGroupPlots(int groupNum, map<int, runInfo> runNumToRunInfo) {
    gr2->SetTitle("Average Maximum VSS Values");
    gr2->GetXaxis()->SetTitle("Number of pages per file");
    gr2->GetYaxis()->SetTitle("Average max VSS value for parameter set (MiB)");
+   gr1->GetXaxis()->CenterTitle(true);
+   gr1->GetYaxis()->CenterTitle(true);
    gr2->Draw("ALP");
 
    // Plot the average number of clusters per file versus the number of pages per file
@@ -213,6 +217,8 @@ void GenerateGroupPlots(int groupNum, map<int, runInfo> runNumToRunInfo) {
    gr3->SetTitle("Average number of Clusters");
    gr3->GetXaxis()->SetTitle("Number of pages per file");
    gr3->GetYaxis()->SetTitle("Number of clusters written to file");
+   gr1->GetXaxis()->CenterTitle(true);
+   gr1->GetYaxis()->CenterTitle(true);
    gr3->Draw("ALP");
 
    // Force the graphics to be rendered to the TCanvas and then save as a pdf
